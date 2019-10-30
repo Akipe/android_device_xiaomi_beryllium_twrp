629fca20f472b96f7aa9107f7b12d57d4ac63f0b
Akipe <code.julien@milletre.fr>
2019-10-30 13:13:27 +0100

    Fix typo and remove unused comment
    Change-Id: I6702e60f37d16ad2532b446585c0521ed3a62524

--- omni_beryllium.mk
+++ omni_beryllium.mk
@@ -3,19 +3,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-
 $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-#$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
-# $(call inherit-product, vendor/omni/config/phone-xxhdpi-4096-dalvik-heap.mk)
-# $(call inherit-product, vendor/omni/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
-
-# Boot animation
-#TARGET_SCREEN_HEIGHT := 2246
-#TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=sdm845
